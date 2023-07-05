14a6b75dca9203304618319995dcdd245c8ebfd1
JuanCruz0715 <juanchi97cruz@gmail.com>
2023-07-05 12:15:46 -0300

    v2.0

--- src/app/detallevehiculos/detallevehiculos.component.ts
+++ src/app/detallevehiculos/detallevehiculos.component.ts
@@ -11,16 +11,20 @@ export class DetallevehiculosComponent implements OnInit {
   vehiculo: any;
   
   constructor(private route: ActivatedRoute,private router: Router) { }
-  
+
   volver() {
     this.router.navigate(['']); // Cambia '/lista-vehiculos' por la ruta correcta de tu lista de vehículos
   }
   ngOnInit() {
     const vehiculos = [
-      { id: 1, tipo: 'auto', marca: 'Toyota', modelo: 'Corolla', imagen: 'assets/images/tc.jpg', anho: 2022, kilometros: 10000, precio: 25000 },
-      { id: 2, tipo: 'moto', marca: 'Honda', modelo: 'Wave', imagen: 'assets/images/hw.jpg', anho: 2021, kilometros: 5000, precio: 15000 },
-      { id: 3, tipo: 'camioneta', marca: 'Ford', modelo: 'F100', imagen: 'assets/images/f100.jpg', anho: 2020, kilometros: 20000, precio: 30000 },
-      
+      { id: 1, tipo: 'auto', marca: 'Toyota', modelo: 'Corolla', imagen: 'assets/images/tc.jpg', anho: 2022, kilometros: 10000, precio: 2500000 },
+      { id: 2, tipo: 'moto', marca: 'Honda', modelo: 'Wave', imagen: 'assets/images/hw.jpg', anho: 2021, kilometros: 5000, precio: 150000 },
+      { id: 3, tipo: 'camioneta', marca: 'Ford', modelo: 'F100', imagen: 'assets/images/f100.jpg', anho: 2004, kilometros: 20000, precio: 400000 },
+      { id: 11, tipo: 'auto', marca: 'volkswagen', modelo: 'gol', imagen: 'assets/images/gl.jpg',anho: 2022, kilometros: 20000, precio: 100000 }
+        
+        
+        
+        
     ];
 
     this.route.paramMap.subscribe(params => { // obtenemos el id
